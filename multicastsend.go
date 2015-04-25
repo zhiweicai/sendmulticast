50a09709ebfa6765b6e1e6e7dd756ab49d15cbd7
zhiweicai <cai.zhiwei@gmail.com>
2015-04-24 21:10:35 -0400

    test

--- multicastsend.go
+++ multicastsend.go
@@ -12,41 +12,51 @@ func main() {
 	var service string
 	var count int
 	var delay int
+	var localaddr string
 
 	count = 1
 	delay = 1
+	localaddr = "127.0.0.1"
 
 	if len(os.Args) > 1 {
 		service = os.Args[1]
 	}
 
 	if len(os.Args) > 2 {
-		count, _ = strconv.Atoi(os.Args[2])
+		localaddr = os.Args[2]
 	}
 
 	if len(os.Args) > 3 {
-		delay, _ = strconv.Atoi(os.Args[3])
+		count, _ = strconv.Atoi(os.Args[3])
+	}
+
+	if len(os.Args) > 4 {
+		delay, _ = strconv.Atoi(os.Args[4])
 	}
 
 	if len(os.Args) > 1 {
-		ping(service, count, delay)
+		ping(service, localaddr, count, delay)
 	}
 
 	os.Exit(0)
 }
 
-func ping(a string, count int, delay int) {
+func ping(a string, b string, count int, delay int) {
 	addr, err := net.ResolveUDPAddr("udp", a)
-	if err != nil {
-		checkError(err)
-	}
-	c, err := net.DialUDP("udp", nil, addr)
+	checkError(err)
+
+	laddr, err := net.ResolveUDPAddr("udp", b)
+	checkError(err)
+	fmt.Println(laddr.String())
+
+	c, err := net.DialUDP("udp", laddr, addr)
 	for i := 0; i < count; i++ {
 		_, err = c.Write([]byte("hello, world\n"))
 		checkError(err)
 		time.Sleep(time.Second * time.Duration(delay))
 	}
 }
+
 func checkError(err error) {
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Fatal error ", err.Error())
